cd976507c66134c898c67911a09949ee40013bd6
vishal-raj15 <vishalraj1380@gmail.com>
2020-01-08 02:20:49 +0530

    logistic .py

---
+++ logistic_digits.py
@@ -0,0 +1,28 @@
+
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+import numpy as np
+
+digits = load_digits()
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(digits.data,digits.target, test_size=0.2)
+
+model.fit(X_train, y_train)
+
+a = model.score(X_test, y_test)
+
+y_predicted = model.predict([digits.data[340]])
+
+
+print("predicted ",y_predicted)
+print('\n')
+print(" real data ",digits.target[340])
+plt.gray()
+plt.matshow(digits.images[340])
+plt.show()
+
